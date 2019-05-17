ddc4cfeaa3c8bfba4f9d6df7b28e27f5f99cbbd2
jeremyk6 <jeremyk6@anatidaepho.be>
2019-05-17 14:56:38 +0200

    La sortie HTML est maintenant réalisée avec la librairie yattag.

--- comparer_profils.py
+++ comparer_profils.py
@@ -2,6 +2,7 @@ from qgis.core import QgsProcessing, QgsProcessingAlgorithm, QgsProcessingMultiS
 from qgis.core import QgsGeometry, QgsVectorLayer, QgsFeature, QgsVectorFileWriter, QgsFields, QgsWkbTypes, QgsCoordinateReferenceSystem, QgsFields, QgsField
 from qgis.PyQt.QtCore import QVariant
 import processing
+from yattag import Doc, indent
 
 class ComparerProfils(QgsProcessingAlgorithm):
 
@@ -19,44 +20,30 @@ class ComparerProfils(QgsProcessingAlgorithm):
         
         # sorties
         output = self.parameterAsFileOutput(parameters, 'OUTPUT', context)
+        doc, tag, text, line = Doc().ttl()
         
         # traitement
-        
-        html = "<html>"
-        html += "<head>"
-        html += '''<style>
-        table {
-          border-collapse: collapse;
-        }
-
-        table, th, td {
-          border: 1px solid black;
-        }
-        </style>
-        '''
-        html += "</head>"
-        html += "<body>"
-        for npont in range(5): # traitement par pont
-            html += "<p>Pont %s<br/>"%npont
-            html += "<table>"
-            html += "<tr>"
-            moyenne_pont = 0
-            for nordre in range(20): # traitement par ordre du point dans le pont
-                pts = []
-                for point_f in points.getFeatures("pont = %s and ordre = %s"%(npont,nordre)): # traitement des 3 points du même ordre
-                    pts.append(point_f.attributes()[-1])
-                moyenne_points = (abs(pts[0]-pts[1])+abs(pts[0]-pts[2])+abs(pts[1]-pts[2]))/3
-                moyenne_pont += moyenne_points
-                html += "<td>%s</td>"%round(moyenne_points,2)
-            html += "</tr>"
-            html += "</table>"
-            moyenne_pont /= 20
-            html += "Moyenne des écarts sur le pont : %s</p>"%round(moyenne_pont,2)
-                
-        html += "</body>"
-        html += "</html>"
+        with tag('html'):
+            with tag('head'):
+                with tag('style'):
+                    text('p, h1 { margin: 10px; } table, th, td { border-collapse: collapse; border: 1px solid black; padding: 5px; }')
+            with tag('body'):
+                for npont in range(5): # traitement par pont
+                    line('h1', 'Pont %s'%npont)
+                    with tag('table'):
+                        with tag('tr'):
+                            moyenne_pont = 0
+                            for nordre in range(20): # traitement par ordre du point dans le pont
+                                pts = []
+                                for point_f in points.getFeatures("pont = %s and ordre = %s"%(npont,nordre)): # traitement des 3 points du même ordre
+                                    pts.append(point_f.attributes()[-1])
+                                moyenne_points = (abs(pts[0]-pts[1])+abs(pts[0]-pts[2])+abs(pts[1]-pts[2]))/3
+                                moyenne_pont += moyenne_points
+                                line('td',str(round(moyenne_points,2)))
+                    moyenne_pont /= 20
+                    line('p', 'Moyenne des écarts sur le pont : %s'%round(moyenne_pont,2))
         file = open(output, "w")
-        file.write(html)
+        file.write(indent(doc.getvalue()))
         file.close()
         
         
