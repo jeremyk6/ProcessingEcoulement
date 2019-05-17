5c31f01a05e6974db9109f237ff35e17e04e22a9
jeremyk6 <jeremyk6@anatidaepho.be>
2019-05-17 16:19:27 +0200

    Séparation traitement/écriture du document HTML et ajout du calcul de l'écart type.

--- comparer_profils.py
+++ comparer_profils.py
@@ -2,6 +2,7 @@ from qgis.core import QgsProcessing, QgsProcessingAlgorithm, QgsProcessingMultiS
 from qgis.core import QgsGeometry, QgsVectorLayer, QgsFeature, QgsVectorFileWriter, QgsFields, QgsWkbTypes, QgsCoordinateReferenceSystem, QgsFields, QgsField
 from qgis.PyQt.QtCore import QVariant
 import processing
+import statistics
 from yattag import Doc, indent
 
 class ComparerProfils(QgsProcessingAlgorithm):
@@ -17,31 +18,47 @@ class ComparerProfils(QgsProcessingAlgorithm):
         
         # entrées
         points = parameters['profils']
+        nombre_ponts = 5
+        nombre_points = 20
         
         # sorties
         output = self.parameterAsFileOutput(parameters, 'OUTPUT', context)
         doc, tag, text, line = Doc().ttl()
         
         # traitement
+        moyennes_ecarts = []
+        ecarts_types = []
+        for npont in range(nombre_ponts): # traitement par pont
+            me = []
+            et = []
+            for nordre in range(nombre_points): # traitement par ordre du point dans le pont
+                pts = []
+                for point_f in points.getFeatures("pont = %s and ordre = %s"%(npont,nordre)): # traitement des 3 points du même ordre
+                    pts.append(point_f.attributes()[-1])
+                me.append((abs(pts[0]-pts[1])+abs(pts[0]-pts[2])+abs(pts[1]-pts[2]))/3) # moyenne des écarts sur le point
+                et.append(statistics.stdev(pts))
+            moyennes_ecarts.append(me)
+            ecarts_types.append(et)
+        
+        # ecriture du document HTML
         with tag('html'):
             with tag('head'):
                 with tag('style'):
                     text('p, h1 { margin: 10px; } table, th, td { border-collapse: collapse; border: 1px solid black; padding: 5px; }')
             with tag('body'):
-                for npont in range(5): # traitement par pont
+                for npont in range(nombre_ponts): # traitement par pont
                     line('h1', 'Pont %s'%npont)
                     with tag('table'):
                         with tag('tr'):
-                            moyenne_pont = 0
-                            for nordre in range(20): # traitement par ordre du point dans le pont
-                                pts = []
-                                for point_f in points.getFeatures("pont = %s and ordre = %s"%(npont,nordre)): # traitement des 3 points du même ordre
-                                    pts.append(point_f.attributes()[-1])
-                                moyenne_points = (abs(pts[0]-pts[1])+abs(pts[0]-pts[2])+abs(pts[1]-pts[2]))/3
-                                moyenne_pont += moyenne_points
-                                line('td',str(round(moyenne_points,2)))
-                    moyenne_pont /= 20
-                    line('p', 'Moyenne des écarts sur le pont : %s'%round(moyenne_pont,2))
+                            line('td', 'Moyennes des écarts')
+                            for nordre in range(nombre_points): # traitement par ordre du point dans le pont
+                                line('td', round(moyennes_ecarts[npont][nordre], 2))
+                        with tag('tr'):
+                            line('td', 'Écarts types')
+                            for nordre in range(nombre_points): # traitement par ordre du point dans le pont
+                                line('td', round(ecarts_types[npont][nordre], 2))
+                    line('p', 'Moyenne des moyennes des écarts sur le pont : %s'%round(statistics.mean(moyennes_ecarts[npont]), 2))
+                    line('p', 'Moyenne des écarts types sur le pont : %s'%round(statistics.mean(ecarts_types[npont]), 2))
         file = open(output, "w")
         file.write(indent(doc.getvalue()))
         file.close()
