fcf04cc5fe78d1e51fcac964d71acce18de42654
jeremyk6 <jeremyk6@anatidaepho.be>
2019-05-21 12:16:58 +0200

    Changement des indicateurs. Préparation complète des tableaux HTML (en-têtes comprises) en amont de leur écriture.

--- comparer_profils.py
+++ comparer_profils.py
@@ -18,29 +18,46 @@ class ComparerProfils(QgsProcessingAlgorithm):
         
         # entrées
         points = parameters['profils']
-        nombre_ponts = 3
-        nombre_points = 20
         
         # sorties
         output = self.parameterAsFileOutput(parameters, 'OUTPUT', context)
         doc, tag, text, line = Doc().ttl()
+        donnees_html = []
+
+        # attributs
+        rasters = points.uniqueValues(points.dataProvider().fields().indexFromName('raster'))
+        ponts = points.uniqueValues(points.dataProvider().fields().indexFromName('pont'))
+        npoints = len(points.uniqueValues(points.dataProvider().fields().indexFromName('ordre')))
         
         # traitement
-        moyennes_ecarts = []
-        ecarts_types = []
-        for npont in range(nombre_ponts): # traitement par pont
-            me = []
-            et = []
-            for nordre in range(nombre_points): # traitement par ordre du point dans le pont
-                pts = []
-                i = 0
-                for point_f in points.getFeatures("pont = %s and ordre = %s"%(npont,nordre)): # traitement des 3 points du même ordre
-                    pts.append(point_f.attributes()[-1])
-                    i += 1
-                me.append((abs(pts[0]-pts[1])+abs(pts[0]-pts[2])+abs(pts[1]-pts[2]))/3) # moyenne des écarts sur le point
-                et.append(statistics.stdev(pts))
-            moyennes_ecarts.append(me)
-            ecarts_types.append(et)
+        tableau = []
+        for pont in ponts: # traitement par pont
+            t_pont = []
+            for raster in rasters:
+                t_raster = []
+                t_raster.append([''])
+                t_raster.append(['Ecart profil 1 - pont'])
+                t_raster.append(['Ecart profil 2 - pont'])
+                t_raster.append(['Ecart profil 1 - profil 2'])
+                for i in range(npoints):
+                    t_raster[0].append(i)
+                t_raster[0].append('Moyenne')
+                t_raster[0].append('Ecart-type')
+                for nordre in range(npoints): # traitement par ordre du point dans le pont
+                    pts = [] # traitement des 3 points du même ordre
+                    for point_f in points.getFeatures("pont = %s and raster = '%s' and ordre = %s"%(pont, raster, nordre)):
+                        pts.append(point_f.attributes()[-1]) 
+                    ecart_p1_pont = (abs(pts[0]-pts[1])) # ecart entre le premier profil et le pont
+                    ecart_p2_pont = (abs(pts[2]-pts[1])) # ecart entre le second profil et le pont
+                    ecart_p1_p2   = (abs(pts[0]-pts[2])) # ecart entre les deux profils
+                    t_raster[1].append(ecart_p1_pont)
+                    t_raster[2].append(ecart_p2_pont)
+                    t_raster[3].append(ecart_p1_p2)
+                t_raster[1] += [statistics.mean(t_raster[1][1:-1]), statistics.stdev(t_raster[1][1:-1])]
+                t_raster[2] += [statistics.mean(t_raster[2][1:-1]), statistics.stdev(t_raster[2][1:-1])]
+                t_raster[3] += [statistics.mean(t_raster[3][1:-1]), statistics.stdev(t_raster[3][1:-1])]
+                t_pont.append(t_raster)
+            tableau.append(t_pont)
         
         # ecriture du document HTML
         with tag('html'):
@@ -48,19 +65,21 @@ class ComparerProfils(QgsProcessingAlgorithm):
                 with tag('style'):
                     text('p, h1 { margin: 10px; } table, th, td { border-collapse: collapse; border: 1px solid black; padding: 5px; }')
             with tag('body'):
-                for npont in range(nombre_ponts): # traitement par pont
-                    line('h1', 'Pont %s'%npont)
-                    with tag('table'):
-                        with tag('tr'):
-                            line('td', 'Moyennes des écarts')
-                            for nordre in range(nombre_points): # traitement par ordre du point dans le pont
-                                line('td', round(moyennes_ecarts[npont][nordre], 2))
-                        with tag('tr'):
-                            line('td', 'Écarts types')
-                            for nordre in range(nombre_points): # traitement par ordre du point dans le pont
-                                line('td', round(ecarts_types[npont][nordre], 2))
-                    line('p', 'Moyenne des moyennes des écarts sur le pont : %s'%round(statistics.mean(moyennes_ecarts[npont]), 2))
-                    line('p', 'Moyenne des écarts types sur le pont : %s'%round(statistics.mean(ecarts_types[npont]), 2))
+                i_pont = 0
+                for pont in ponts: # traitement par pont
+                    line('h1', 'Pont %s'%pont)
+                    i_raster = 0
+                    for raster in rasters:
+                        line('h2', raster)
+                        with tag('table'):
+                            for ligne in tableau[i_pont][i_raster]:
+                                with tag('tr'):
+                                    for cellule in ligne:
+                                        if type(cellule) is not str:
+                                            cellule = str(round(cellule, 2)) 
+                                        line('td', cellule)
+                        i_raster += 1
+                    i_pont += 1
         file = open(output, "w")
         file.write(indent(doc.getvalue()))
         file.close()
